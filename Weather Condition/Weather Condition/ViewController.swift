3c31805db80022edc66a6fd26cb23ac64a6d83ed
RAN <kumar.ranbijay@gmail.com>
2016-06-04 17:50:11 +0530

    Weather Condition app in Swift - Enter city name and check temperature details.

---
+++ Weather Condition/Weather Condition/ViewController.swift
@@ -0,0 +1,106 @@
+//
+//  ViewController.swift
+//  Weather Condition
+//
+//  Created by RANBIJAY KUMAR on 04/06/16.
+//  Copyright © 2016 SaturnMob. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var userCity: UITextField!
+
+    
+    @IBAction func findWeather(sender: AnyObject) {
+        
+        var url = NSURL(string: "http://www.weather-forecast.com/locations/" + userCity.text!.stringByReplacingOccurrencesOfString(" ", withString: "-") + "/forecasts/latest")
+        
+        if url != nil {
+            
+            let task = NSURLSession.sharedSession().dataTaskWithURL(url!, completionHandler: { (data, response, error) -> Void in
+                
+                var urlError = false
+                
+                var weather = ""
+                
+                if error == nil {
+                    
+                    var urlContent = NSString(data: data!, encoding: NSUTF8StringEncoding) as NSString!
+                    
+                    var urlContentArray = urlContent.componentsSeparatedByString("<span class=\"phrase\">")
+                    
+                    if urlContentArray.count > 0 {
+                        
+                        var weatherArray = urlContentArray[1].componentsSeparatedByString("</span>")
+                        
+                        weather = weatherArray[0] as! String
+                        
+                        weather = weather.stringByReplacingOccurrencesOfString("&deg;", withString: "º")
+                        
+                    } else {
+                        
+                        urlError = true
+                        
+                    }
+                    
+                    
+                    
+                } else {
+                    
+                    urlError = true
+                    
+                }
+                
+                dispatch_async(dispatch_get_main_queue()) {
+                    
+                    if urlError == true {
+                        
+                        self.showError()
+                        
+                    } else {
+                        
+                        self.resultLabel.text = weather
+                        
+                    }
+                }
+                
+            })
+            
+            task.resume()
+            
+            
+        } else {
+            
+            showError()
+            
+        }
+        
+        
+    }
+    
+    @IBOutlet var resultLabel: UILabel!
+    
+    func showError() {
+        
+        resultLabel.text = "Was not able to find weather for " + userCity.text! + ". Please try again"
+        
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
